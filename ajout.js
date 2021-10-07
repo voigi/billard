47f20a3f31bfab74a3d89b5ba514dabb3caec160
voigi <supervoigi@gmail.com>
2021-10-07 13:49:16 +0200

    ajout Date + enumeration Joueur

--- ajout.js
+++ ajout.js
@@ -2,32 +2,54 @@
 
 
 
-function handler (){ 
+function handler() {
 
-    var input = document.getElementById("text").value;
-    var list = document.getElementById("list");
-    var printOutput = document.createElement('li');
-    
-    if(input != ''){
-   printOutput.appendChild(document.createTextNode(input));   
-   list.appendChild(printOutput);
-    }
-    
+  var input = document.getElementById("text").value;
+  var list = document.getElementById("list");
+  var printOutput = document.createElement('li');
+  // var buttonOutput = document.createElement('button');
+  // buttonOutput.classList.add('btn');  
+  // buttonOutput.classList.add('btn-danger');
+
+
+  if (input != '') {
+    printOutput.appendChild(document.createTextNode(input));
+    //  buttonOutput.appendChild(document.createTextNode('Effacer'));    
+    list.appendChild(printOutput);
+    //  printOutput.appendChild(buttonOutput);
   }
 
-function effacer (){
-      var input = document.getElementById("text").value;
-      input = document.getElementById("text").value='';
 }
-function tooltip(){
-    
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
-});
+
+
+function effacer() {
+  var input = document.getElementById("text").value;
+  input = document.getElementById("text").value = '';
+}
+
+function tooltip() {
+
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 
 }
-  tooltip();
-  document.getElementById("submit").addEventListener('click', handler);
-  document.getElementById("reset").addEventListener('click', effacer);
-  document.getElementById("text").addEventListener('keypress', function(e){ (e.which == 13) && handler();});
+
+
+
+var items = [];
+function guardarNumeros() {
+
+  var input = document.getElementById("text").value;
+  items.push(input);
+  console.log(items);
+  return false;
+}
+
+
+
+tooltip();
+document.getElementById("submit").addEventListener('click', handler);
+document.getElementById("reset").addEventListener('click', effacer);
+document.getElementById("text").addEventListener('keypress', function (e) { (e.which == 13) && handler(); });
 
